81ad4e9ea39736d7fc4f5f7eb68fb6d54a0f1a70
yosephfetene <155078686+yosephfetene@users.noreply.github.com>
2025-04-13 10:20:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked");
-    });
-}
+//for (var i = 0; i < numberOfDrumButtons; i++) {
+    //document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        //alert("I got clicked");
+    //});
+//}
 
 document.addEventListener('keypress', gono);
 
