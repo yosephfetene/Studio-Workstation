b77439c459020e3943be8ff0c16b2d61dcee4bd6
yosephfetene <155078686+yosephfetene@users.noreply.github.com>
2025-04-14 20:18:20 +0300

    transformed the project fully
    changed the project from Drumkit to workstation

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".instrument").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", gono2);
+    document.querySelectorAll(".instrument")[i].addEventListener("click", gono2);
 }
 
 function gono2(event){
     var buttoninnerhtml = this.innerHTML;
     switch (buttoninnerhtml){
+        case "W":
         case "w": 
             let sound = new Audio("floor-tom.mp3");
             sound.play();
             break;
+        case "A":
         case "a":
             let sound1 = new Audio("tom-2.mp3");
             sound1.play();
             break;
+        case "S":
         case "s":
             let sound2 = new Audio("tsnare-drum.mp3");
             sound2.play();
             break;
+        case "D":
         case "d":
             let sound3 = new Audio("whoosh-drum.mp3");
             sound3.play();
             break;
+        case "J":
         case "j":
             let sound4 = new Audio("tr707-crash-cymbal.mp3");
             sound4.play();
             break;
+        case "K":
         case "k":
             let sound5 = new Audio("long-ride-cymbal.mp3");
             sound5.play();
             break;
+        case "L":
         case "l":
             let sound6 = new Audio("kick-drum.mp3");
             sound6.play();
